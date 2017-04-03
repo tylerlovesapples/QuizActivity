4a75b435cc15a782edd733c83976fdf03b906071
tylerthomas <tylerthomas1171@gmail.com>
2017-04-03 12:56:58 -0500

    Good God

---
+++ app/src/main/java/edu/andrews/cptr252/tjthomas/quiz/QuizActivity.java
@@ -0,0 +1,48 @@
+package edu.andrews.cptr252.tjthomas.quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.Console;
+
+public class QuizActivity extends AppCompatActivity {
+
+   //Start quiz button
+   private Button mStartButton;
+
+   //Show the question list
+   private Button mListButton;
+
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_quiz);
+
+      //get reference to
+      mStartButton = (Button) findViewById(R.id.startQuiz);
+
+      //get reference to
+      mListButton = (Button) findViewById(R.id.questionList);
+
+      /*
+      Set onClick listener to respond when the user taps the button
+       */
+      mStartButton.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            System.out.print("mStartButton clicked");
+         }
+      });
+
+      mListButton.setOnClickListener(new View.OnClickListener(){
+         @Override
+         public void onClick(View v) {
+            System.out.print("mListButton clicked");
+         }
+      });
+   }
+}
